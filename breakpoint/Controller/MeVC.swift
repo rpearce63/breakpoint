edcd7a0ff1c29dfa19dc6eebc2d09ebd395ee0ce
Rick Pearce <rpearce63@yahoo.com>
2018-04-09 07:23:08 -0500

    profile page and MeVC

---
+++ breakpoint/Controller/MeVC.swift
@@ -0,0 +1,26 @@
+//
+//  MeVC.swift
+//  breakpoint
+//
+//  Created by Rick on 4/9/18.
+//  Copyright © 2018 Pearce. All rights reserved.
+//
+
+import UIKit
+
+class MeVC: UIViewController {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction func signOutBtnPressed(_ sender: Any) {
+        
+    }
+    
+}
