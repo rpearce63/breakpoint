011063b676ee9ef91880306d6cdc73ee5275e506
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-10 15:58:23 -0500

    add group feed view

---
+++ breakpoint/Controller/GroupFeedVC.swift
@@ -0,0 +1,32 @@
+//
+//  GroupFeedVC.swift
+//  breakpoint
+//
+//  Created by Rick Pearce on 4/10/18.
+//  Copyright © 2018 Pearce. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedVC: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    @IBOutlet weak var groupTitleLbl: UILabel!
+    @IBOutlet weak var membersLbl: UILabel!
+    @IBOutlet weak var sendBtnView: UIView!
+    @IBOutlet weak var messageTextField: insetTextField!
+    @IBOutlet weak var sendBtn: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        sendBtnView.bindToKeyboard()
+    }
+
+    @IBAction func backBtnPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func sendBtnPressed(_ sender: Any) {
+        
+    }
+}
---
+++ breakpoint/View/GroupFeedCell.swift
@@ -0,0 +1,23 @@
+//
+//  GroupFeedCell.swift
+//  breakpoint
+//
+//  Created by Rick Pearce on 4/10/18.
+//  Copyright © 2018 Pearce. All rights reserved.
+//
+
+import UIKit
+
+class GroupFeedCell: UITableViewCell {
+
+    @IBOutlet weak var profileImage: UIImageView!
+    @IBOutlet weak var emailLbl: UILabel!
+    @IBOutlet weak var contentLbl: UILabel!
+    
+
+    func configureCell(profileImage: UIImage, email: String, content: String) {
+        self.profileImage.image = profileImage
+        self.emailLbl.text = email
+        self.contentLbl.text = content
+    }
+}
