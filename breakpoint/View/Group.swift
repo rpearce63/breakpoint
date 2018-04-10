35fe32894a156e388c958d41d03165ebb2974a99
Rick Pearce <rick.pearce.crzs@statefarm.com>
2018-04-10 14:59:56 -0500

    pull groups from Firebase and populate table

---
+++ breakpoint/View/Group.swift
@@ -0,0 +1,52 @@
+//
+//  Group.swift
+//  breakpoint
+//
+//  Created by Rick Pearce on 4/10/18.
+//  Copyright © 2018 Pearce. All rights reserved.
+//
+
+import Foundation
+
+class Group {
+    
+    private var _groupTitle: String
+    private var _groupDesc: String
+    private var _key: String
+    private var _memberCount: Int
+    private var _members: [String]
+    
+    var groupTitle : String {
+        return _groupTitle
+    }
+    
+    var groupDesc : String {
+        return _groupDesc
+    }
+    
+    var key: String {
+        return _key
+    }
+    
+    var memberCount : Int {
+        return _memberCount
+    }
+    
+    var members : [String] {
+        return _members
+    }
+    
+    init(title: String, description: String, key: String, members: [String], memberCount: Int) {
+        self._groupTitle = title
+        self._groupDesc = description
+        self._key = key
+        self._members = members
+        self._memberCount = memberCount
+    }
+}
+
+
+
+
+
+
